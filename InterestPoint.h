c31743201924708312447b48c5b1deea0cfd9011
jsmvalente <jvalente96@gmail.com>
2015-11-26 19:39:52 +0000

    Update InterestPoint.h

--- InterestPoint.h
+++ InterestPoint.h
@@ -5,7 +5,7 @@ typedef struct interestpoint InterestPoint;
 Array GraphDecoderInit(char ***, int, int, int, int);
 char GetIP_Type(int, Array);
 int GetIP_Coord(int, char, Array);
-int* FindIP(int, int, int, int, Array);
+int FindIP(int, int, int, int, Array);
 void OccupyPos(int, Array, int);
 void FreePos(int, Array, int);
 int GetFlagRes(int, Array);
