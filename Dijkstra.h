6da1d889e288d8ab4cd1a9e6212b257d20ff482e
jsmvalente <jvalente96@gmail.com>
2015-11-29 16:36:42 +0000

    Update Dijkstra.h

--- Dijkstra.h
+++ Dijkstra.h
@@ -1,2 +1,5 @@
 int Comparison(Item, Item);
 void Dijsktra(Graph *, int, int*, int*, int);
+void PathCalculator(Graph *, int, ListNode**, ListNode**, Array, ListNode *, char, int);
+ListNode * CreatePathListBackwards(ListNode*, int*, int);
+ListNode * CreatePathList(ListNode*, int*, int);
