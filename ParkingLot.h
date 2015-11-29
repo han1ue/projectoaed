8cc3de7383bcd9b84af57662140904440930a8bd
jsmvalente <jvalente96@gmail.com>
2015-11-29 16:15:15 +0000

    Ups, forgot the .h

--- ParkingLot.h
+++ ParkingLot.h
@@ -5,7 +5,7 @@ typedef struct parkinglot ParkingLot;
 ParkingLot * InitParkingLot( FILE *, int, int, int, int);
 ParkingLot * MapInit(FILE *);
 char *** MatrixInit(int*, int*, FILE *, int, int, int);
-int * InitAccesses(int, Array, int);
+ListNode * InitAccesses(int, Array, int);
 int * InitRamps(int, Array, int);
 void FreeMatrix(char ***, int, int, int);
 Graph* GetGraph(ParkingLot*);
